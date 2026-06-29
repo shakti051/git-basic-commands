60b65cc00aa6eb62bbda6217d9eccc2ebe58b716
shakti051 <shaktithemastermind@gmail.com>
2026-06-29 20:50:28 +0530

    profile UI started

---
+++ lib/profile.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Profile extends StatefulWidget {
+  const Profile({super.key});
+
+  @override
+  State<Profile> createState() => _ProfileState();
+}
+
+class _ProfileState extends State<Profile> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    body: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text("Profile section start")
+    ],),
+    );
+  }
+}
